ced08941e53368824af53542b466709fa5a0c69e
Aspak Shaikh <shaikhashfak159@gmail.com>
2026-03-30 12:59:47 +0530

    fix CORS with wildcard vercel pattern

--- src/main/java/com/zeon/type_server/config/SecurityConfig.java
+++ src/main/java/com/zeon/type_server/config/SecurityConfig.java
@@ -62,8 +62,7 @@ public class SecurityConfig {
                 "http://localhost:5173",
                 "http://127.0.0.1:3000",
                 "http://127.0.0.1:5173",
-                "multitype-3lq0vn9eg-ashfak11s-projects.vercel.app",
-                "https://multitype.vercel.app"
+                "https://*.vercel.app"
         ));
 
         configuration.setAllowedMethods(List.of(
